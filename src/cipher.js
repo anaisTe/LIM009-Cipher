a1fde7a07d86146210f4964389e05e1897cdfb8e
TCS.Campomanes Porras <gcampomanesp@proveedor.intercorp.com.pe>
2019-02-24 10:00:51 -0500

    cipher finish

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,24 @@
 window.cipher = {
-  encode: () => {
+  encode: (offset, string) => {
     /* Acá va tu código */
+    let text_result = '';
+    for (let i = 0; i < string.length; i++) {
+      let number = texto_ingresado.charCodeAt(i);
+      if(number >= 65 && number <= 90) {
+        let desplazamiento = parseInt(offset);
+        let result = (number-65+desplazamiento)%26+65;
+        let sum = String.fromCharCode(result); 
+        text_result += sum;
+      }
+      if(number === 32){
+        text_result += ' ';
+      }
+      
+    }
+    return text_result;
   },
   decode: () => {
     /* Acá va tu código */
+    
   }
-};
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-//que el boton vamos tenga funcionalidad
-function botonCifrar(){
+let texto_ingresado;
+let des;
+let result = 0;
+
+document.getElementById("botVamos").addEventListener("click", divCifrar);
+
+function divCifrar(){
   document.getElementById("divCifrar").className += "show"; 
 }
+
+document.getElementById("cipher").addEventListener("click", cipherText);
+
 function cipherText(){
-  const lol = document.getElementById("areaTexto").value;
-  alert(lol);
+  document.getElementById("area2").value = '';
+
+  texto_ingresado = document.getElementById("area1").value.toUpperCase();
+  des = document.getElementById("desplazamiento").value;
+  const result = cipher.encode(des, texto_ingresado);
+  document.getElementById("area2").value = result;
 }
 
+document.getElementById('clean').addEventListener("click", function() {
+  document.getElementById("area1").value = "";
+  document.getElementById("area2").value = "";
+  document.getElementById("desplazamiento").value = "";
+});
